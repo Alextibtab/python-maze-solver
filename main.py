d859cbfac94d60a7d6c32448b507857428008161
Alextibtab <14alexcockburn@gmail.com>
2022-11-15 00:27:59 +0000

    Testing new classes.

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from maze_solver.graphics import Window
+from maze_solver.graphics import Window, Point, Line
 
 
 if __name__ == "__main__":
+    a = Point(10, 10)
+    b = Point(100, 100)
+    line = Line(a, b)
+
     win = Window(800, 600, "Maze Solver!")
+    win.draw_line(line, "black")
     win.wait_for_close()
