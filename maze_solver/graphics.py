5ac69e7a536b4e514bfc02b9645b7f2593f3d986
Alextibtab <14alexcockburn@gmail.com>
2022-11-15 00:26:06 +0000

    Added Point and Line classes and modified Window class to support drawing of lines.

--- maze_solver/graphics.py
+++ maze_solver/graphics.py
@@ -16,6 +16,9 @@ class Window:
         self.__root.update_idletasks()
         self.__root.update()
 
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
+
     def wait_for_close(self):
         self.__running = True
         while self.__running:
@@ -23,3 +26,32 @@ class Window:
 
     def close(self):
         self.__running = False
+
+
+class Point:
+    def __init__(self, x, y):
+        self.__x = x
+        self.__y = y
+
+    def get_x(self):
+        return self.__x
+
+    def get_y(self):
+        return self.__y
+
+
+class Line:
+    def __init__(self, start_point, end_point):
+        self.__start = start_point
+        self.__end = end_point
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.__start.get_x(),
+            self.__start.get_y(),
+            self.__end.get_x(),
+            self.__end.get_y(),
+            fill=fill_color,
+            width=2,
+        )
+        canvas.pack(fill=BOTH, expand=1)
